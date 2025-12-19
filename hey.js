af556bfcc21d1af20f7c3d380feddedf60279e39
vaishnavi <vaishnavi.mishra67009@gmail.com>
2025-12-19 17:45:02 +0530

    added new code

---
+++ hey.js
@@ -0,0 +1,7 @@
+var typed=new Typed (".text",{
+    strings: ["Frontend Developer","UI/UX Designer","web developer"],
+    typespeed: 70,
+    backspeed: 100,
+    backDelay: 1000,
+    loop:true,
+});
