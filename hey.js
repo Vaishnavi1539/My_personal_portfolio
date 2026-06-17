a25da05a7404d57f1e348193ef6cabeee089d254
vaishnavi <vaishnavi.mishra67009@gmail.com>
2026-06-18 05:09:23 +0530

    fix navbar scroll issue

--- hey.js
+++ hey.js
@@ -1,5 +1,5 @@
 var typed=new Typed (".text",{
-    strings: ["Frontend Developer","UI/UX Designer","web developer"],
+    strings: ["Frontend Developer","React developer","UI/UX Designer"],
     typespeed: 70,
     backspeed: 100,
     backDelay: 1000,
